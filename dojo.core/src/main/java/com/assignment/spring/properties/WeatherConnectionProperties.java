893b9e0724922a7fe0fca511cd6031ed0e438691
Mateusz <mateusz.rybarkiewicz@gmail.com>
2022-01-30 22:39:01 +0100

    Added validation on configuration

--- dojo.core/src/main/java/com/assignment/spring/properties/WeatherConnectionProperties.java
+++ dojo.core/src/main/java/com/assignment/spring/properties/WeatherConnectionProperties.java
@@ -3,9 +3,16 @@ package com.assignment.spring.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "weather")
 @Data
+@Validated
 public class WeatherConnectionProperties {
+
+    @NotBlank
     private String apiKey;
+
 }
