02631414d23b897af5ad0c27c8e5381e88426eef
Mateusz <mateusz.rybarkiewicz@gmail.com>
2022-02-01 16:11:33 +0100

    Added lombok to persistence module, refactored code a bit

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
 Spring Boot Coding Dojo
 ---
 
-Welcome to the Spring Boot Coding Dojo!
-
-### Introduction
-
-This is a simple application that requests its data from [OpenWeather](https://openweathermap.org/) and stores the
-result in a database. The current implementation has quite a few problems making it a non-production ready product.
-
-### The task
-
-As the new engineer leading this project, your first task is to make it production-grade, feel free to refactor any
-piece necessary to achieve the goal.
-
 ### How to run the code
 
 - Build application with mvn clean install
@@ -22,9 +10,3 @@ piece necessary to achieve the goal.
     - java -jar dojo.application/target/dojo.application-0.0.1-SNAPSHOT.jar
 - Send sample request
   > curl --location --request GET 'localhost:8080/weather?city=moscow'
-
-> **DO NOT create a Pull Request with your solution**
-
-### Footnote
-
-It's possible to generate the API key going to the [OpenWeather Sign up](https://openweathermap.org/appid) page.
--- dojo.persistence/src/main/java/com/assignment/spring/entities/WeatherEntity.java
+++ dojo.persistence/src/main/java/com/assignment/spring/entities/WeatherEntity.java
@@ -1,5 +1,7 @@
 package com.assignment.spring.entities;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +10,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "weather")
+@Data
 public class WeatherEntity {
 
     @Id
@@ -15,40 +18,6 @@ public class WeatherEntity {
     private Integer id;
 
     private String city;
-
     private String country;
-
     private Double temperature;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(Double temperature) {
-        this.temperature = temperature;
-    }
 }
